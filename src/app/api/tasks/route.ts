d1f3f4b14ff1692441a6c8dfd80becd9ec678e9a
hikoyara <hikoyara7@icloud.com>
2024-05-14 23:24:49 +0900

    ローディングコンポーネント 基礎学習

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -10,7 +10,10 @@ const tasks: Task[] = [
     { id: 2, name: "ランニング" },
 ];
 
+const sleep = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
+
 export const GET = async () => {
+    await sleep(3000);
     return NextResponse.json({ tasks }, { status: 200 });
 };
 
